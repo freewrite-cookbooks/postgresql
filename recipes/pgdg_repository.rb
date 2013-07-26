899e0fe6c66e839eca73f1450504e041437248f2
Danny Guinther <danny@backupify.com>
2013-07-26 11:23:52 -0400

    Conditionalize apt_repo creation

--- recipes/pgdg_repository.rb
+++ recipes/pgdg_repository.rb
@@ -5,5 +5,6 @@ apt_repository 'apt.postgresql.org' do
 	components ['main']
 	distribution "#{node.lsb.codename}-pgdg"
 	key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+	not_if 'apt-cache policy | grep -q apt.postgresql.org/pub/repos/apt'
 	uri 'http://apt.postgresql.org/pub/repos/apt'
 end

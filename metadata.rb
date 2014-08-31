3a7f0dd1ea031b4bb95c4ac2b93921a74d4997ce
Danny Guinther <dannyguinther@gmail.com>
2014-08-31 09:11:02 -0400

    Change version to 1.0.0 to meet database cookbook requirement

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'dev@freewrite.org'
 license           'MIT'
 description       'Postgresql installation, cluster management, and configuration management, for Debian-based systems.'
 long_description  'Postgresql installation, cluster management, and configuration management, for Debian-based systems.'
-version           '0.0.1'
+version           '1.0.0'
 
 recipe 'postgresql', 'Noop'
 recipe 'postgresql::client', 'Installs psql client using the appropriate installation method for the platform.'

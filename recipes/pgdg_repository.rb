371f5eee36846fbcc77b792f46f5a1b51bbea0d2
Danny Guinther <dguinther@seismic.com>
2018-01-08 10:24:36 -0500

    Update PGDG key URL

--- recipes/pgdg_repository.rb
+++ recipes/pgdg_repository.rb
@@ -4,7 +4,7 @@ apt_repository 'apt.postgresql.org' do
 	action :add
 	components ['main']
 	distribution "#{node.lsb.codename}-pgdg"
-	key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+	key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
 	not_if 'apt-cache policy | grep -q apt.postgresql.org/pub/repos/apt'
 	uri 'http://apt.postgresql.org/pub/repos/apt'
 end
